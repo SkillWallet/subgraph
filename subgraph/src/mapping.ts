95f439596441cb3eb02bea18918623ef022ac0c3
blagojDimovski <blagoj.dimovski@yandex.com>
2021-04-05 15:04:08 +0200

    updates

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -6,21 +6,64 @@ import {
   SkillSetUpdated,
   SkillWalletCommunityChanged
 } from "../generated/SkillWallet/SkillWallet"
-import { SkillWallet, Community, Member, SkillSet, Skill } from "../generated/schema"
+import { SkillWallet, Community, SkillSet, Skill } from "../generated/schema"
 
 
 export function handleCreate(event: SkillWalletCreated): void {
 
+  let skillWalletOwnerString = event.params.skillWalletOwner.toHexString()
+  let communityAddressString = event.params.community.toHexString()
+  let tokenId = event.params.skillWalletId.toHex()
+  let skillSet = event.params.skillSet;
+
+  // TODO: Parse skillSet into Skill and wrap it into SkillSet
+
+  let skillWallet = new SkillWallet(tokenId);
+
+  skillWallet.tokenId = event.params.skillWalletId;
+  skillWallet.owner = event.params.skillWalletOwner;
+  skillWallet.activeCommunity = communityAddressString;
+  skillWallet.communityHistory.push(communityAddressString);
+  skillWallet.skillSet = skillSet;
+  skillWallet.activated = false;
+  skillWallet.createdAt = event.block.timestamp
+
+  skillWallet.save();
 }
 
 export function handleActivate(event: SkillWalletActivated): void {
+
+  let tokenId = event.params.skillWalletId.toHex()
+
+  let skillWallet = SkillWallet.load(tokenId);
+  skillWallet.activated = true;
+  skillWallet.save();
 
 }
 
 export function handleSkillSetUpdate(event: SkillSetUpdated): void {
 
+  let tokenId = event.params.skillWalletId.toHex()
+  let newSkillSet = event.params.newSkillSet;
+
+  let skillWallet = SkillWallet.load(tokenId);
+
+  skillWallet.skillSet = newSkillSet;
+
+  skillWallet.save();
+
 }
 
 export function handleCommunityChange(event: SkillWalletCommunityChanged): void {
+
+  let tokenId = event.params.skillWalletId.toHex()
+  let newCommunityAddressString = event.params.community.toHexString()
+
+  let skillWallet = SkillWallet.load(tokenId);
+
+  skillWallet.activeCommunity = newCommunityAddressString;
+  skillWallet.communityHistory.push(newCommunityAddressString);
+
+  skillWallet.save();
 
 }
